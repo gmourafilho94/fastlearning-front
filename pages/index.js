f3c39bd1f6c357e99dc1d6afd02485d2fda27b1c
Gabriel Ribeiro <gmourafiho94@gmail.com>
2025-11-26 17:36:59 -0300

    wip - root layout

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Education should be free to all</h1>
+  return (
+    <div className="catch-sentence">
+      <h1>Education should be free to all</h1>
+    </div>
+  );
 }
 
 export default Home;
---
+++ pages/layout.js
@@ -0,0 +1,11 @@
+function MainLayout({ children }) {
+  return (
+    <html lang="br">
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
+
+export default MainLayout;
